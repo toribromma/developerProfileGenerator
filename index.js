e8c8aef59747df118b7e1852a3712bea80ee8d07
Tori Bromma <toribromma@gmail.com>
2022-05-06 21:36:03 -0700

    axios call to github for data

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const util = require("util");
 const template = require("./generateHTML");
-const {green, blue, pink, red} = template.colors;
-const generateHTML = template.generateHTML
+const { green, blue, pink, red } = template.colors;
+const generateHTML = template.generateHTML;
 const questions = [
   {
     type: "input",
@@ -15,11 +19,55 @@ const questions = [
   },
 ];
 
+function promptUser() {
+  return inquirer.prompt(questions);
+}
+
+function getUserInfo(username) {
+  const queryUrl = `https://api.github.com/users/${username}`;
+  return axios.get(queryUrl);
+}
+
 function writeToFile(fileName, data) {}
 
 async function init() {
-console.log(green);
-console.log(generateHTML)
+  //   console.log(green);
+  //   console.log(generateHTML);
+
+  try {
+    const answers = await promptUser();
+    // console.log(answers);
+    const { username, color } = answers;
+    const res = await getUserInfo(username);
+    // console.log(data);
+    const {
+      avatar_url,
+      name,
+      location,
+      html_url,
+      blog,
+      bio,
+      public_repos,
+      followers,
+      following,
+    } = res.data;
+
+    console.log(
+      avatar_url,
+      name,
+      location,
+      html_url,
+      blog,
+      bio,
+      public_repos,
+      followers,
+      following
+    );
+
+
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 init();
