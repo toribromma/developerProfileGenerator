46377945397d5bba5d7b1934aa5212a22824aa66
Tori Bromma <toribromma@gmail.com>
2022-05-06 20:45:27 -0700

    added questions

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const questions = [
-  
+  {
+    type: "input",
+    name: "username",
+    message: "Type in your github username",
+  },
+  {
+    message: "What is your favorite color?",
+    name: "color",
+    type: "list",
+    choices: ["green", "blue", "pink", "red"],
+  },
 ];
 
-function writeToFile(fileName, data) {
- 
-}
+function writeToFile(fileName, data) {}
 
-function init() {
+async function init() {}
 
 init();
